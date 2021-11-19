eda2ef45110d2b49d535636dce4ca2efb6757dc3
geetha846 <geethageetu846@gmail.com>
2021-11-19 14:24:01 +0530

    main.py,main1.py r sample scripts

---
+++ main.py
@@ -0,0 +1,11 @@
+n=int(input("Enter a number:"))
+t=num
+r=0
+while(n>0):
+    d=n%10
+    rev=rev*10+d
+    n=n//10
+if(t==rev):
+    print("The number is palindrome!")
+else:
+    print("Not a palindrome!")
